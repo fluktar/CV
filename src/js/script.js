a8591ac80a6200a30389388bab9e98d1016d42d7
Paweł <tyc.pawel@gmail.com>
2023-07-10 18:48:34 +0200

    update

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,13 @@
-"use strict";
+'use strict';
 function login(form) {
-    const username = form.username.value;
-    const password = form.password.value;
+	const username = form.username.value;
+	const password = form.password.value;
 
-    if(username === 'ptyc' && password === 'ptyc1@3') {
-        document.getElementById('loginForm').style.display = 'none';
-    }
+	if (username === 'ptyc' && password === 'ptyc1@3') {
+		document.getElementById('loginForm').style.display = 'none';
+	} else {
+		document.getElementById('errorDiv').style.display = 'block';
+	}
 
-    return false;
+	return false;
 }
-
