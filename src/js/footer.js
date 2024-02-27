4ee69bd35434183a334d3eaed9d34d24e712cfa5
Paweł <tyc.pawel@gmail.com>
2024-02-27 22:38:45 +0100

    update

---
+++ src/js/footer.js
@@ -0,0 +1,4 @@
+"use strict";
+const year = document.getElementById("year");
+const date = new Date();
+year.textContent = date.getFullYear();
--- src/js/script.js
+++ src/js/script.js
@@ -3,22 +3,47 @@ let loggedIn = false;
 const loginForm = document.getElementById("loginForm");
 
 const login = "ptyc";
-const password = "ptyc1@3";
+const password = "ptyc123";
 
 const loginInput = document.getElementById("username");
+
 const passwordInput = document.getElementById("password");
 
+const errorDiv = document.getElementById("errorDiv");
+
 const pl = document.getElementById("pl");
+
 const eng = document.getElementById("eng");
 
 const loginButton = document.getElementById("loginButton");
 
-function btn() {
-  loginButton.addEventListener("click", function () {
-    loginForm.style.display = "none";
+function showLoginForm() {
+  if (loginInput.value === login && passwordInput.value === password) {
+    loggedIn = true;
+  }
+  if (loggedIn) {
+    if (pl.checked && eng.checked) {
+      pl.checked = false;
+    } else if (pl.checked) {
+      window.location.href = "pl.html";
+    } else if (eng.checked) {
+      window.location.href = "eng.html";
+    }
+  } else {
+    errorDiv.style.display = "block";
+  }
+
+  pl.addEventListener("click", function () {
+    if (pl.checked && eng.checked) {
+      eng.checked = false;
+    }
+  });
+
+  eng.addEventListener("click", function () {
+    if (pl.checked && eng.checked) {
+      pl.checked = false;
+    }
   });
 }
 
-if (loginInput.value == login) {
-  btn();
-}
+loginButton.addEventListener("click", showLoginForm);
